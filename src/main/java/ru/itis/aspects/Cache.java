92c979ee7d0707c226be6ef582a6c4dfc7a0d6c6
SUDPro <stepan.kurin@yandex.ru>
2019-09-11 15:34:21 +0300

    delete extra lines

--- src/main/java/ru/itis/aspects/Cache.java
+++ src/main/java/ru/itis/aspects/Cache.java
@@ -1,18 +1,12 @@
 package ru.itis.aspects;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import ru.itis.entities.Driver;
-import ru.itis.entities.Trip;
-import ru.itis.entities.User;
 import ru.itis.repository.postgres.DriverRepository;
-import ru.itis.repository.postgres.TripRepository;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -46,7 +40,4 @@ public class Cache {
             return driverOptional;
         }
     }
-    
-
-
 }
